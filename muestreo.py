320df214286ebadc780a5c9324897c2776784770
madperez <mperez@ittg.edu.mx>
2020-12-08 10:36:00 -0600

    verifica si la altura cambia en un dia

--- muestreo.py
+++ muestreo.py
@@ -11,11 +11,11 @@ import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
 
-img=cv.imread('/home/nautica/Documents/investigacion/images/estres/Profuda0.jpg',0)
 # se tomaron 3 muestras de cada planta
 # se asume que las plantas que se encuentran en cada columna fueron regadas de la misma forma
 # posteriormente se deben tomar muestras basadas en la segmentación y mas de un solo pixel
 # bien regadas
+# las coordenadas se obtuvieron usando matplotlib
 plantas_left=[[166,35],[186,73],[219,49],[136,145],[135,138],[160,105],[149,230],[138,149],[149,305]]
 # regadas suficientemente
 plantas_center=[[336,86],[351,129],[283,155],[323,271],[340,230],[291,245],[274,156],[307,66],[192,136]]
@@ -28,6 +28,7 @@ distancia=[]
 tiempo=[]
 etiqueta=[]
 for t in range(73):
+    img=cv.imread('/home/nautica/Documents/investigacion/images/estres/Profuda'+str(t)+'.jpg',0)
     for col,ren in plantas_left:
         renglon.append(ren)
         columna.append(col)
@@ -48,7 +49,7 @@ for t in range(73):
         etiqueta.append('mal regada')
 df = pd.DataFrame(list(zip(renglon, columna,distancia,tiempo,etiqueta)), columns =['renglon', 'columna','distancia','tiempo','etiqueta'])
 print(df.head())
-# verifica si la altura cambia en el trancurso de un dia
+# verifica si la altura cambia en el trancurso de un dia para el punto 166,35
 variacion_altura_al_dia=df[(df['renglon']==35)&(df['columna']==166)]
 variacion_altura_al_dia.plot(x="tiempo", y=["distancia"])
 #cv.imshow('imagen de profundidad',img)
